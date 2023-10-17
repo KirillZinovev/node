cc690c221638780bd2a01f4274f0dbc6da954975
Зиновьев Кирилл <kirillzinovev23@gmail.com>
2023-10-17 15:00:31 +0300

    commit 2

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
-// const { fstat } = require("fs");
-const path = require("path");
-const fs = require("fs");
-// console.log("название файла:", path.basename(__filename));
-// console.log("название каталога:", path.dirname(__filename));
-// console.log("расширение:", path.extname(__filename));
+import express from "express";
+const app = express();
+const port = "3000";
 
-// fs.mkdir(path.join(__dirname, "tmp"), function (err) {
-//   if (err) {
-//     console.error(err);
-//   }
-//   console.log("папка создана");
-// });
+app.get("/as", function (req, res) {
+  res.end("hello world");
+  console.log(res);
+});
 
-const filePath = path.join(__dirname, "tmp", "2.txt");
-console.log(filePath);
-fs.writeFile(filePath, "somthing wrong in your file", function (err) {
-  if (err) {
-    console.error(err);
-  }
-});
-fs.appendFile(filePath, "\n somthing wrong in your file", function (err) {
-  if (err) {
-    console.error(err);
-  }
-});
-fs.readFile(filePath, "UTF-8", (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(data);
+app.listen(port, () => {
+  console.log("listening on port", +port);
 });
